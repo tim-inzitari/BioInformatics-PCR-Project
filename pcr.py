88426edfaeda7e908fdacc23cc1a7d7f17a0c734
Tim Inzitari <tsi3@zips.uakron.edu>
2020-09-15 17:07:45 -0400

    Implement find_compliment

--- pcr.py
+++ pcr.py
@@ -4,10 +4,20 @@
 
 
 
-def find_compliment(RNA):
+def find_compliment(seq):
 	#compliment the sequence
-	DNA = ""
-	return DNA
+
+	#dictionary for complimenting
+	compliment = {'A' : 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
+
+	#set original sequence to a list
+	bases = list(seq)
+
+	##replace each iteration against the dictionary
+	bases = [compliment[base] for base in bases]
+
+	#join back into a string
+	return ''.join(bases)
 
 
 ## Original Code for our section, nsp1 from https://www.ncbi.nlm.nih.gov/nuccore/NC_045512
@@ -18,6 +28,8 @@ with open('genome.txt', 'r') as file:
 	RNA = RNA.upper()
 
 	DNA = find_compliment(RNA)
+
+	# print(DNA) for testing the Find_Compliment
 
 	#get R primer and F primer (from Blast)
 	# R being the reverse of F
