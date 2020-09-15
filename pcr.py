b32539bb1729d0e3891479bfc0c47d3ee341cd5e
Tim Inzitari <tsi3@zips.uakron.edu>
2020-09-15 16:24:44 -0400

    added code for our selected genome

--- pcr.py
+++ pcr.py
@@ -1,3 +1,6 @@
 ## PCR Project
 # Group 5  (nsp1)
 # Tim Inzitari, Andrew Robinson, Weston Jessie
+
+
+nsp1_Orig = "atgga gagccttgtc cctggtttca acgagaaaac acacgtccaa ctcagtttgc ctgttttaca ggttcgcgac gtgctcgtac gtggctttgg agactccgtg gaggaggtct tatcagaggc acgtcaacat cttaaagatg gcacttgtgg cttagtagaa gttgaaaaag gcgttttgcc tcaacttgaa cagccctatg tgttcatcaa acgttcggat gctcgaactg cacctcatgg tcatgttatg gttgagctgg tagcagaact cgaaggcatt cagtacggtc gtagtggtga gacacttggt gtccttgtcc ctcatgtggg cgaaatacca gtggcttacc gcaaggttct tcttcgtaag aacggtaata aaggagctgg tggccatagt tacggcgccg atctaaagtc atttgactta ggcgacgagc ttggcactga tccttatgaa gattttcaag aaaactggaa cactaaacat agcagtggtg ttacccgtga actcatgcgt gagcttaacg gaggg"
