bc4fd8fe4cd816cabef0190375d3744ecc12eb46
Tim Inzitari <tsi3@zips.uakron.edu>
2020-09-15 16:52:28 -0400

    Add template for find compliment

--- pcr.py
+++ pcr.py
@@ -2,6 +2,14 @@
 # Group 5  (nsp1)
 # Tim Inzitari, Andrew Robinson, Weston Jessie
 
+
+
+def find_compliment(RNA):
+	#compliment the sequence
+	DNA = ""
+	return DNA
+
+
 ## Original Code for our section, nsp1 from https://www.ncbi.nlm.nih.gov/nuccore/NC_045512
 with open('genome.txt', 'r') as file:
 	RNA = file.read()
@@ -9,7 +17,7 @@ with open('genome.txt', 'r') as file:
 	#convert to uppercase
 	RNA = RNA.upper()
 
-	#DNA = find_compliment(RNA)
+	DNA = find_compliment(RNA)
 
 	#get R primer and F primer (from Blast)
 	# R being the reverse of F
