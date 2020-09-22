a9f0fabfeea8577bfa03807fbc6dc277c62f5ffa
Andrew Robinson <alr104@zips.uakron.edu>
2020-09-22 15:03:01 -0400

    Update pcr.py
    Add PCR simulation

--- pcr.py
+++ pcr.py
@@ -1,35 +1,61 @@
 ## PCR Project
 # Group 5  (nsp1)
 # Tim Inzitari, Andrew Robinson, Weston Jessie
+from random import randint
+import matplotlib.pyplot as plt
 
-
+# Parameters
+cycles = 20
+hangOffAllowance = 0
+copyLength = 200
+e = 50
 
 def find_compliment(RNA):
-	#compliment the sequence
-
-	#dictionary for complimenting
-	compliment = {'A' : 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
-
-	#set original sequence to a list
-	bases = list(RNA)
-
-	##replace each iteration against the dictionary
-	bases = [compliment[base] for base in bases]
-
-	#join back into a string
-	#and return as tuple (RNA, cDNA)
-	return (RNA,''.join(bases))
+	f = RNA.replace('A','x')
+	f = f.replace('T','A')
+	f = f.replace('x','T')
+	f = f.replace('C','x')
+	f = f.replace('G','C')
+	f = f.replace('x','G')
+	return f
 
 
 def pcr_run(DNA, fPrimer, rPrimer, cycles):
 	#Run PCR
 	return
+
+def andrew_pcr_run(forwardStrands,reverseStrands,fPrimer,rPrimer):
+
+	forwardSize = len(forwardStrands)
+	reverseSize = len(reverseStrands)
+
+	for x in range(0,forwardSize):
+		index = forwardStrands[x].find(fPrimer)
+		if index != -1:
+			newStrand = forwardStrands[x][index:index+20+min(copyLength+randint(-e,e),len(forwardStrands[x])-(index+20))]
+			newStrand = find_compliment(newStrand)
+			newStrand = newStrand[::-1]
+			reverseStrands.append(newStrand)
+
+	for x in range(0,reverseSize):
+		index = reverseStrands[x].find(rPrimer)
+		if index != -1:
+			newStrand = reverseStrands[x][index:index+20+min(copyLength+randint(-e,e),len(reverseStrands[x])-(index+20))]
+			newStrand = find_compliment(newStrand)
+			newStrand = newStrand[::-1]
+			forwardStrands.append(newStrand)
+
+	return;
+
 ## Original Code for our section, nsp1 from https://www.ncbi.nlm.nih.gov/nuccore/NC_045512
 with open('genome.txt', 'r') as file:
+
 	RNA = file.read()
 
 	#convert to uppercase
 	RNA = RNA.upper()
+	cDNA = find_compliment(RNA)
+	cDNA = cDNA[::-1]
 
 	#Tuple (RNA, cDNA)
 	DNA = find_compliment(RNA)
@@ -40,17 +66,43 @@ with open('genome.txt', 'r') as file:
 	# R being the reverse of F
 	# Format ("Sequence", Starting Point, Ending Point, GC Content")
 
+	# get a random sub string of size 150-250
+	rsRNA = RNA[randint(25,75):randint(225,275)]
+	print("Random subregion size: " + str(len(rsRNA))+"\n")
+
 	#Primer pair #2
-	fPrimer = ("TCGTACGTGGCTTTGGAGAC", 80, 99, 0.55)
-	rPrimer = ("AGATCGGCGCCGTAACTATG", 419, 400, 0.55)
+	fPrimer = (rsRNA[0:20], 81, 100, 0.55)
+	rPrimer = (find_compliment(rsRNA[len(rsRNA)-20:len(rsRNA)]), 246, 227, 0.55)
 
-	#Print the sequences to replicate
-	# should be 340 long
-	print("\nReplicating: \n\nForward:")
-	print(DNA[0][fPrimer[1]:rPrimer[1]])
-	print("\nReverse:")
-	print(DNA[1][fPrimer[1]:rPrimer[1]])
+	# #Print the sequences to replicate
+	# # should be 340 long
+	# print("\nReplicating: \n\nForward:")
+	# print(RNA[fPrimer[1]:rPrimer[1]])
+	# print("\nReverse:")
+	# print(DNA[fPrimer[1]:rPrimer[1]])
 
-	#run the replication function for X Cycles
-	#set right now to 20
-	replicated_DNA = pcr_run(DNA, fPrimer, rPrimer, cycles=30)
+	# Doing this so that I can read all strands left to right
+	forwardStrands = []
+	forwardStrands.append(RNA)
+	reverseStrands = []
+	reverseStrands.append(cDNA)
+
+	# Same reason as above
+	fPrimerA = fPrimer[0]
+	rPrimerA = rPrimer[0][::-1]
+
+	for _ in range(cycles):
+		andrew_pcr_run(forwardStrands,reverseStrands,fPrimerA,rPrimerA)
+
+	print("Total forward: "+str(len(forwardStrands))+"\n")
+	print("Total reverse: "+str(len(reverseStrands)))
+	lengths = []
+
+	for strand in forwardStrands:
+		lengths.append(len(strand))
+
+	for strand in reverseStrands:
+		lengths.append(len(strand))
+
+	plt.hist(x, bins= 10)
+	plt.show()
